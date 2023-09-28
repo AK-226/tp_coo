a0a1a7b748dbe404d2f28b277153e404cfa3b539
Aoua Kanazoe <knw3255a@u1-201-01d.univ-tlse3.fr>
2023-09-28 17:29:04 +0200

    ajouter les fichier

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -8,56 +8,57 @@ class Departement(models.Model):
     def __str__(self):
         return f"Departement{self.numero}"
 
+class Machine(models.Model):
+    nom = models.CharField(max_length=200)
+    prix =models.IntegerField()
+
+    def __str__(self):
+        return self.nom
+class Ingredient(models.Model):
+    nom = models.CharField(max_length=200)
+
+    def __str__(self):
+     return self.nom
+
+class QuantiteIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantite = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.quantite} {self.ingredient.nom}"
+
+class Action(models.Model):
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+    commande = models.CharField(max_length=200)
+    duree = models.IntegerField()
+    ingredient = models.ManyToManyField(QuantiteIngredient)
+    action_parent = models.ForeignKey("self", on_delete=models.CASCADE, blank=True, null=True, related_name="actions_children")
+
+    def __str__(self):
+        return f"Action {self.commande} sur la machine {self.machine.nom}"
+
+class Recette(models.Model):
+    nom = models.CharField(max_length=200)
+    action = models.ForeignKey(Action, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nom
+
 class Usine(models.Model):
     departement = models.ForeignKey(Departement, on_delete=models.CASCADE)
     taille = models.CharField(max_length=200)
-    machines = models.IntegerField()
-    recettes = models.CharField(max_length=200)
-    stocks = models.IntegerField()
+    machines = models.ManyToManyField(Machine)
+    recettes = models.ManyToManyField(Recette)
+    stocks = models.ManyToManyField(QuantiteIngredient)
 
-    def _str_(self):
+    def __str__(self):
         return f"Usine dans le département{self.departement.numero}"
 
-class Ingredient(models.Model):
-    nom = models.CharField(max_length= 200)
-
-    def _str_(self):
-     return f"Nom de l'ingredient{self.ingredient.nom}"
 
 class Prix(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE,related_name="+")
     departement = models.ForeignKey(Departement, on_delete=models.CASCADE,related_name="+")
     prix = models.IntegerField()
 
-    def _str_(self):
+    def __str__(self):
         return f"Prix pour les ingrédients {self.ingredient} dans le département {self.departement.numero}"
-
-class QuantiteIngredient(models.Model):
-    ingredient = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
-    quantite = models.IntegerField()
-
-    def _str_(self):
-        return f"{self.quantite} {self.ingredient.nom}"
-
-class Machine(models.Model):
-    nom = models.CharField(max_length=200)
-
-    def _str_(self):
-        return self.nom
-
-class Action(models.Model):
-    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
-    commande = models.CharField(max_length=200)
-    duree = models.IntegerField()
-    ingredients = models.CharField(Ingredients)
-    action = models.CharField(max_length=200)
-
-    def _str_(self):
-        return f"Action {self.commande} sur la machine {self.machine.nom}"
-
-class Recette(models.Model):
-    nom = models.CharField(max_length=100)
-    action = models.ManyToManyField('Action')
-
-    def _str_(self):
-        return self.nom
--- brewsim/high_level/tests.py
+++ brewsim/high_level/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import machine
+
+Class MachineModelTests(TestCase):
+def test_usine_creation(self):
+    self.assertEqual(Machine.objects.count(), 0)
+    Machine.objects.create(nom="four", prix=1_000)
+    self.asssertEqual(Machine.objects.count(), 1)
+
+    
